1f7d3c5841323bc36fdd96108e440461118c8663
viethq <gg4acrossover@gmail.com>
2017-05-20 10:49:23 +0700

    update

---
+++ Project 03 -OnAPIClient/Project 03 -OnAPIClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 03 -OnAPIClient
+//
+//  Created by viethq on 5/20/17.
+//  Copyright © 2017 viethq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
